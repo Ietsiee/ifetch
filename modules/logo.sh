ab83377665bd88e3bce8b4d783152e21e58969f3
Ietsiee <309194441+Ietsiee@users.noreply.github.com>
2026-07-28 20:30:45 +0200

    Added finding correct logo.txt for module logo

--- modules/logo.sh
+++ modules/logo.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
-cat /etc/ifetch/logo.txt
+
+#Find logo file
+if [ -f "$HOME/.config/ifetch/logo.txt" ]; then
+    logo="$HOME/.config/ifetch/logo.txt"
+else
+    logo="/etc/ifetch/logo.txt"
+fi
+
+cat "$logo"
