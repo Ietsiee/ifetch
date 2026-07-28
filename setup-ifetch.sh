4465fbf8a3bea4433b74f512b03b4fb21371db02
Ietsiee <309194441+Ietsiee@users.noreply.github.com>
2026-07-28 20:45:16 +0200

    Update setup-ifetch.sh

--- setup-ifetch.sh
+++ setup-ifetch.sh
@@ -12,24 +12,35 @@ if command -v ifetch >/dev/null 2>&1; then
 fi
 
 echo "Cloning ifetch..."
-git clone -q https://github.com/Ietsiee/ifetch.git
+git clone -q https://github.com/Ietsiee/ifetch
 
 cd ifetch
 
 chmod +x modules/*
 chmod +x ifetch.sh
-chmod +x ifetch-launcher.sh
+chmod +x ifetch-launcher.sh    
 
 echo "Installing ifetch..."
 sudo cp ifetch-launcher.sh /bin/ifetch
+
 sudo mkdir -p /etc/ifetch
 sudo cp ifetch.sh /etc/ifetch/ifetch.sh
+sudo cp -r modules /etc/ifetch/modules
 sudo cp help.txt /etc/ifetch/help.txt
-cp logo.txt "$HOME/.config/ifetch/logo.txt"
 sudo cp logo.txt /etc/ifetch/logo.txt
 sudo cp ifetch.config /etc/ifetch/ifetch.config
-sudo cp ifetch.config "$HOME/.config/ifetch/ifetch.config"
-sudo cp -r modules /etc/ifetch/modules
+
+mkdir -p "$HOME/.config/ifetch"
+if [ -f "$HOME/.config/ifetch/ifetch.config" ]; then
+    echo "ifetch.config already exists"
+else
+    cp ifetch.config "$HOME/.config/ifetch/ifetch.config"
+fi
+if [ -f "$HOME/.config/ifetch/logo.txt" ]; then
+    echo "logo.txt already exists"
+else
+    cp logo.txt "$HOME/.config/ifetch/logo.txt"
+fi
 
 echo "Cleaning up..."
 cd ..
