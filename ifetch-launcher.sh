2d34e35c68835068f05d66f9f0f98ed325d25362
Ietsiee <309194441+Ietsiee@users.noreply.github.com>
2026-07-29 18:08:47 +0200

    Update ifetch-launcher.sh

--- ifetch-launcher.sh
+++ ifetch-launcher.sh
@@ -10,6 +10,19 @@ case "$1" in
     -u|--update)
         curl -fsSL https://raw.githubusercontent.com/Ietsiee/ifetch/main/setup-ifetch.sh | sh
         ;;
+    -d|--default)
+        echo "Do you want to reset your user config to the default? (y/N)"
+        echo -n "> "
+        read answer
+
+        if [ "$answer" = "y" ] || [ "$answer" = "Y" ]; then
+            cp /etc/ifetch/ifetch.config "$HOME/.config/ifetch.config"
+            echo "Done"
+            ifetch
+        else
+            echo "Cancelled"
+        fi
+        ;;
     "")
         /etc/ifetch/ifetch.sh
         ;;
